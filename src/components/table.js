ce1657c10b9f43ee2985dc1b83270a79a07360eb
marin4ka <w0rt0x@yandex.ru>
2025-12-02 23:54:50 +0200

    commit

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,45 +8,46 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-    before.forEach(subName => { // перебираем нужный массив идентификаторов
-        root[subName] = cloneTemplate(subName); // клонируем и получаем объект, сохраняем в таблице
-        root.container.append(root[subName].container); // добавляем к таблице после (append) или до (prepend)
-    });
-    after.forEach(subName => {
-        root[subName] = cloneTemplate(subName);
-        root.container.append(root[subName].container);
+  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  before.forEach((subName) => {
+    // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName); // клонируем и получаем объект, сохраняем в таблице
+    root.container.append(root[subName].container); // добавляем к таблице после (append) или до (prepend)
+  });
+  after.forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.append(root[subName].container);
+  });
+
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+  root.container.addEventListener("change", () => {
+    onAction();
+  });
+  root.container.addEventListener("reset", () => {
+    setTimeout(onAction, 100);
+  });
+  root.container.addEventListener("submit", (e) => {
+    e.preventDefault();
+    onAction(e.submitter);
+  });
+
+  const render = (data) => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+      Object.keys(item).forEach((key) => {
+        if (key in row.elements) {
+          row.elements[key].textContent = item[key];
+        }
+      });
+      return row.container;
     });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
-    root.container.addEventListener('change', () => {
-        onAction();
-    });
-    root.container.addEventListener('reset', () => {
-        setTimeout(onAction, 100)
-    });
-    root.container.addEventListener('submit', (e) => {
-        e.preventDefault();
-        onAction(e.submitter);
-    });
+    root.elements.rows.replaceChildren(...nextRows);
+  };
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = data.map(item => {
-            const row = cloneTemplate(rowTemplate);
-            Object.keys(item).forEach(key => {
-                if (key in row.elements) {
-                    row.elements[key].textContent = item[key];
-                }
-            });
-            return row.container;
-        });
-
-        root.elements.rows.replaceChildren(...nextRows);
-    }
-
-    return {...root, render};
+  return { ...root, render };
 }
